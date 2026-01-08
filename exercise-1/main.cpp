31acd9d7bbd7339d0987396b2eaaa99f46374d5b
Kimi Leinola <154047539+Onsekyl@users.noreply.github.com>
2026-01-08 12:09:45 +0200

    Exercise 1 otherwise ready but I'm not sure if the input needs to accept integer only or not.

--- exercise-1/main.cpp
+++ exercise-1/main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
+int game(int maxNum);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int gameResult = game(20);
+    cout << "Oikea vastaus! Arvauksia: " << gameResult << endl;
+
     return 0;
+}
+
+int game(int maxNum)
+{
+    int targetNum, guessNum = -1, guessAmount = 0;
+
+    srand(time(NULL)); // Unix timestamp for the randomisation's seed
+    targetNum = rand() % maxNum; // '% value' is the upper limit
+
+    while (guessNum != targetNum)
+    {
+        guessAmount++;
+
+        cout << "Arvaa luku: ";
+
+        // ?? Should the input accept integer only??
+        cin >> guessNum;
+
+        if (guessNum < targetNum)
+        {
+            cout << "luku on suurempi kuin " << guessNum << endl;
+        }
+        else if (guessNum > targetNum)
+        {
+            cout << "luku on pienempi kuin " << guessNum << endl;
+        }
+
+        cout << endl;
+    }
+
+    return guessAmount;
 }
