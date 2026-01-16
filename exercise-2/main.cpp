569887c9b6c50befdb959a9ac6aa84df33359b3f
Kimi Leinola <154047539+Onsekyl@users.noreply.github.com>
2026-01-16 09:18:09 +0200

    Add second exercise project.

---
+++ exercise-2/main.cpp
@@ -0,0 +1,13 @@
+/*
+Katso UML-luokkakaaviot ja Mittari-luentoesimerkki moodlesta.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
