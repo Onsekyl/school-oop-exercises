4d92d69aac8a21608e2325cf457e5197501b0d3d
Kimi Leinola <154047539+Onsekyl@users.noreply.github.com>
2026-01-28 20:32:39 +0200

    exercise-4: customer.h contents

--- exercise-4/customer.h
+++ exercise-4/customer.h
@@ -1,11 +1,28 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
 
+#include <iostream>
+#include "creditaccount.h"
+
+using namespace std;
 
 class Customer
 {
 public:
-    Customer();
+    Customer(string ownerName, double creditAmount);
+    string getName();
+    void showBalance();
+    bool deposit(double);
+    bool withdraw(double);
+    bool creditDeposit(double);
+    bool creditWithdraw(double);
+
+
+private:
+    string name;
+    BankAccount currentAccount;
+    CreditAccount creditAccount;
+
 };
 
 #endif // CUSTOMER_H
